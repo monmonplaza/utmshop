a22ef0fed503d4a41dedcd9e6d8882bac11a765d
monmonplaza <monmon.plaza@gmail.com>
2021-12-27 14:41:55 +0800

    first commit

---
+++ src/components/Header.js
@@ -0,0 +1,87 @@
+import React from "react";
+import { RiUser3Fill, RiShoppingCart2Fill } from "react-icons/ri";
+import { GoGlobe } from "react-icons/go";
+import {
+  AiFillFacebook,
+  AiOutlineTwitter,
+  AiFillLinkedin,
+  AiFillYoutube,
+} from "react-icons/ai";
+import { Link } from "react-router-dom";
+const Header = () => {
+  return (
+    <>
+      <header className="header ">
+        <div className="header__top d--flex bg--primary">
+          <div className="container">
+            <ul className="d--flex">
+              <li>
+                <RiUser3Fill /> <Link to="/">Login</Link> /{" "}
+                <Link to="/">Register</Link>
+              </li>
+              <li>
+                <button>
+                  <RiShoppingCart2Fill />
+                </button>
+              </li>
+            </ul>
+          </div>
+        </div>
+        <div className="header__main">
+          <div className="container">
+            <div className="header__main__bottom d-grid">
+              <img src="./images/logo.png" alt="" />
+              <nav>
+                <ul className=" d--flex">
+                  <li>
+                    <Link to="/">Home</Link>
+                  </li>
+                  <li>
+                    <Link to="/">Shop</Link>
+                  </li>
+                  <li>
+                    <Link to="/">Checkout</Link>
+                  </li>
+                </ul>
+              </nav>
+
+              <ul className="socialmedia d--flex">
+                <li>
+                  <a href="#">
+                    <AiFillFacebook />
+                  </a>
+                </li>
+
+                <li>
+                  <a href="#">
+                    <AiOutlineTwitter />
+                  </a>
+                </li>
+
+                <li>
+                  <a href="#">
+                    <AiFillLinkedin />
+                  </a>
+                </li>
+
+                <li>
+                  <a href="#">
+                    <AiFillYoutube />
+                  </a>
+                </li>
+
+                <li>
+                  <a href="#">
+                    <GoGlobe />
+                  </a>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </header>
+    </>
+  );
+};
+
+export default Header;
---
+++ src/components/Hero.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Hero = () => {
+  return (
+    <>
+      <div className="hero">
+        <div className="container"></div>
+      </div>
+    </>
+  );
+};
+
+export default Hero;
